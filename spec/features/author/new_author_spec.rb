4babaf5e65ba2ae4314865664ada52bd3bf9722a
Simon Cyrani <simon.cyrani@student.hpi.de>
2021-11-14 19:23:43 +0100

    implemented adding of authors into db

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
 describe "New author page", type: :feature do
+  before {visit new_author_path}
   it "should exist at 'new_author_path' and render withour error" do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
-    visit new_author_path
   end
   it "should have text inputs for first & last name and a homepage of an author" do
-    visit new_author_path
     expect(page).to have_field('author[first_name]')
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
+  end
+  it "should insert a new author in the database after submitting a filled form" do
+    page.fill_in 'author[first_name]', with: 'Alan'
+    page.fill_in 'author[last_name]', with: 'Turing'
+    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+    page.find('input[type="submit"]').click
   end
 end
