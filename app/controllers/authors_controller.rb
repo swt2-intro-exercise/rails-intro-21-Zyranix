c642a71b748fc02b00868a4577947158ce847aee
Simon Cyrani <simon.cyrani@student.hpi.de>
2021-11-15 03:41:16 +0100

    implemented edit actions fixes #10

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,27 +1,42 @@
 class AuthorsController < ApplicationController
-    def new
-        @author = Author.new
-    end
+  def new
+    @author = Author.new
+  end
 
-    def create
-        @author = Author.new(author_params)
-        if @author.save
-            redirect_to authors_path
-        else
-            render 'new'
-        end
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to authors_path
+    else
+      render "new"
     end
+  end
 
-    def show
-        @author = Author.find(params[:id])
+  def show
+    @author = Author.find(params[:id])
+  end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    @author = Author.find(params[:id])
+   
+    if @author.update(author_params)
+      redirect_to @author
+    else
+      render 'edit'
     end
+  end
 
-    def index
-        @authors = Author.all
-    end
+  def index
+    @authors = Author.all
+  end
 
-    private
-        def author_params
-            params.require(:author).permit(:first_name,:last_name,:homepage)
-        end
+  private
+
+  def author_params
+    params.require(:author).permit(:first_name, :last_name, :homepage)
+  end
 end
